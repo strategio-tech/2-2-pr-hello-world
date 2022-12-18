4a7b31af362e932e2de0bc5e5b920a89fcfab073
deepti-tri <100620410+deepti-tri@users.noreply.github.com>
2022-12-17 19:13:50 -0600

    Updated test to pass on Windows
    Changed line separator to a universal one, due to issues with Gradle test on local windows machines.

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -19,7 +19,7 @@ public class TestHello {
      Hello.main(null);
 
      // assertion
-     assertEquals("Hello world!\n", bos.toString());
+     assertEquals("Hello world!" + System.lineSeparator(), bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
